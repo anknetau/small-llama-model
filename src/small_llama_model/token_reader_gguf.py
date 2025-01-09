4c0477bc8ddf9993c44240f98e5dcd84a274dc6d
Andres Kievsky <andres.kievsky@gmail.com>
2025-01-10 01:38:34 +1100

    Cleanup

--- src/small_llama_model/token_reader_gguf.py
+++ src/small_llama_model/token_reader_gguf.py
@@ -4,11 +4,11 @@ from tokens import Special, Specials, GToken, GTType, AToken
 from model import Model
 from typing import Any, Callable
 
-# tokenizer.ggml.model 'llama'
+# This file implements the GGML tokeniser within GGUF
+
+# https://github.com/ggerganov/ggml/blob/master/docs/gguf.md
+
 # tokenizer.ggml.pre 'default'
-# tokenizer.ggml.tokens ['<unk>', '<s>', '</s>', '<0x00>', '<0x01>', '<0x02>', '<0x03>', '<0x04>', '<0x05>', '<0x06>', '<0x0
-# tokenizer.ggml.scores [-1000.0, -1000.0, -1000.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.
-# tokenizer.ggml.token_type [3, 3, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 
 
 PREFIX = "tokenizer.ggml."
 
@@ -16,6 +16,9 @@ def read(model: Model):
     named: Callable[[str], str] = lambda s: PREFIX + s
     info: Callable[[str], Any] = lambda s: model.info[named(s)]
     info_int: Callable[[str], int] = lambda s: model.info[named(s)]
+
+    tokenizer_name = info("model")
+    assert(tokenizer_name == "llama")
 
     tokens: list[str] = info("tokens")
     token_types: list[int] = info("token_type")
@@ -30,6 +33,9 @@ def read(model: Model):
     unknown = makeSpecial(gtokens[info_int("unknown_token_id")])
     start = makeSpecial(gtokens[info_int("bos_token_id")])
     end = makeSpecial(gtokens[info_int("eos_token_id")])
+    # Not used:
+    # tokenizer.ggml.separator_token_id: uint32: Separator token
+    # tokenizer.ggml.padding_token_id: uint32: Padding token
 
     specials = Specials(
         unknown = unknown,
