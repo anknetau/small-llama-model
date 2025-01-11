7551566e0354cd3e874a2f8a37955e2e29848ab3
Andres Kievsky <andres.kievsky@gmail.com>
2025-01-11 23:42:04 +1100

    File loading cleanup

--- src/small_llama_model/run.py
+++ src/small_llama_model/run.py
@@ -10,52 +10,62 @@ from utils.common import *
 from third.llama3 import LlamaThirdParty
 from utils.prompts import prompt_cs, prompt_text
 
+@dataclass
+class Runner:
+    model: Model
+    bpe: BPE
+    prompt: str = ""
+
+    @classmethod
+    def make(cls, loader: Callable[[], tuple[Model, BPE, str]]):
+        (model, bpe, prompt) = loader()
+        return Runner(model, bpe, prompt)
+
+    @classmethod
+    def load_llama(cls):
+        print("* Loading Llama")
+        with open(Constants.MODEL_LLAMA_39, "rb") as reader:
+            llama_model = Model.load(reader)
+        assert_check_model(llama_model)
+        bpe = BPE()
+        bpe.read(token_reader_impl.GGUFReader(llama_model))
+        assert_check_bpe(bpe)
+        return llama_model, bpe, prompt_text()
+
+    @classmethod
+    def load_flcc(cls):
+        print("* Loading FLCC")
+        with open(Constants.MODEL_FLCC_CS, "rb") as reader:
+            flcc_model = Model.load(reader)
+        assert_check_model(flcc_model)
+        bpe = BPE()
+        with open(Constants.BPE_FLCC_CS, 'r') as reader:
+            bpe.read(token_reader_impl.NumericLinesReader(reader))
+        assert_check_bpe(bpe)
+        return flcc_model, bpe, prompt_cs()
+
+    def check(self, useThirdParty: bool=False):
+        prompt = self.prompt
+        print("input:", str.encode(prompt))
+        tokens = self.bpe.encode(prompt, self.model.embedding_length, fill=False, start=True, end=False)
+        print("tokens:", tokens)
+        if useThirdParty:
+            bpe = self.bpe
+            llama_third = LlamaThirdParty(self.model, bpe)
+            llama_third.run_pass(tokens, bpe)
+        else:
+            bpe = self.bpe
+            result = self.model.run_pass(tokens)
+            print("result:", bpe.decode_token(result))
+
+
 def start():
-    check(*load_llama())
-    check(*load_flcc())
-    check(*load_llama(), True)
-
-
-# checks
-
-def check(model: Model, bpe: BPE, prompt: str, useThirdParty: bool=False):
-    print("input:", str.encode(prompt))
-    tokens = bpe.encode(prompt, model.embedding_length, fill=False, start=True, end=False)
-    print("tokens:", tokens)
-    if useThirdParty:
-        llama_third = LlamaThirdParty(model, bpe)
-        llama_third.run_pass(tokens, bpe)
-    else:
-        result = model.run_pass(tokens)
-        print("result:", bpe.decode_token(result))
-
-# Loaders
-
-def load_llama():
-    print("* Loaded Llama")
-    with open(Constants.MODEL_LLAMA_39, "rb") as reader:
-        llama_model = Model.load(reader)
-    assert_check_model(llama_model)
-    bpe = BPE()
-    bpe.read(token_reader_impl.GGUFReader(llama_model))
-    assert_check_bpe(bpe)
-    return llama_model, bpe, prompt_text()
-
-def load_flcc():
-    print("* Loaded FLCC")
-    with open(Constants.MODEL_FLCC_CS, "rb") as reader:
-        flcc_model = Model.load(reader)
-    assert_check_model(flcc_model)
-    bpe = BPE()
-    with open(Constants.BPE_FLCC_CS, 'r') as reader:
-        bpe.read(token_reader_impl.NumericLinesReader(reader))
-    assert_check_bpe(bpe)
-    return flcc_model, bpe, prompt_cs()
-
+    Runner.make(Runner.load_llama).check()
+    Runner.make(Runner.load_flcc).check()
+    Runner.make(Runner.load_llama).check(True)
 
 if __name__ == '__main__':
     start()
-
 
     # print(flcc_model.detailed_description())
 
